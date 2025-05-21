db1a654852324c2aa5b91775b0f1b3e1dcfd3cfe
kwon-h-chan <a71179447@gmail.com>
2025-05-21 10:23:40 +0900

    Initial commit

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/P20250521/UHT/P20250521.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeP20250521_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_P20250521;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_P20250521()
+	{
+		if (!Z_Registration_Info_UPackage__Script_P20250521.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/P20250521",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xB47E2D83,
+				0xDAF4777B,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_P20250521.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_P20250521.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_P20250521(Z_Construct_UPackage__Script_P20250521, TEXT("/Script/P20250521"), Z_Registration_Info_UPackage__Script_P20250521, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB47E2D83, 0xDAF4777B));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/P20250521/P20250521.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "P20250521.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, P20250521, "P20250521" );
